ed1c78f6492c382bb29d4a2c02c33fc9ceb62d94
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2026-04-20 09:59:50 +0000

    Merge two set of node coordinates

--- src/grid_manipulation/paste_grids.py
+++ src/grid_manipulation/paste_grids.py
@@ -2,6 +2,30 @@ import numpy as np
 import porepy as pp
 
 
+def merge_node_coords(g1, g2, plane_coefficients, offset):
+    nodes_1 = find_nodes_on_surface(g1, plane_coefficients, offset)
+    nodes_2 = find_nodes_on_surface(g2, plane_coefficients, offset)
+
+    _, mapped_1, mapped_2 = match_nodes(g1.nodes[:, nodes_1], g2.nodes[:, nodes_2])
+
+    assert np.allclose(mapped_1, np.arange(mapped_1.size))
+
+    reduced_nodes_2 = np.delete(g2.nodes, nodes_2, axis=1)
+
+    ind_2 = g1.num_nodes + np.arange(g2.nodes.shape[1])
+    reduction = np.cumsum(np.isin(np.arange(g2.num_nodes), nodes_2))
+    ind_2 -= reduction
+    for i in range(len(mapped_2)):
+        # This is a weak spot, I have not tested towards on a case where the ordering
+        # of the nodes is different in the two grids. This could give rise to a mapping
+        # error.
+        ind_2[nodes_2[mapped_2[i]]] = nodes_1[mapped_1[i]]
+
+    assert ind_2.size == np.unique(ind_2).size
+
+    return np.hstack((g1.nodes, reduced_nodes_2)), nodes_1, nodes_2, ind_2
+
+
 def match_nodes(n_1, n_2):
     num_nodes = n_1.shape[1]
     assert n_2.shape[1] == num_nodes
--- tests/test_paste_grids.py
+++ tests/test_paste_grids.py
@@ -16,6 +16,28 @@ def test_match_nodes(seed):
     np.testing.assert_allclose(n2, common[:, mapped_2])
 
 
+@pytest.mark.parametrize("nx", np.array([[1, 1, 1], [2, 2, 2]]))
+@pytest.mark.parametrize("active_dim", [0, 1, 2])
+def test_merge_node_coords(nx, active_dim):
+    g1 = pp.StructuredTetrahedralGrid(nx, [1, 1, 1])
+    g2 = pp.StructuredTetrahedralGrid(nx, [1, 1, 1])
+    offset = 1
+
+    g2.nodes[active_dim, :] += offset
+
+    plane_coefficients = np.zeros(3)
+    plane_coefficients[active_dim] = 1
+    nodes, _, _, ind_2 = paste_grids.merge_node_coords(
+        g1, g2, plane_coefficients, offset
+    )
+
+    passive_dim = np.delete(np.arange(3), active_dim)
+    num_overlapping_nodes = np.prod(nx[passive_dim] + 1)
+    assert nodes.shape[1] == g1.num_nodes + g2.num_nodes - num_overlapping_nodes
+    np.testing.assert_allclose(nodes[:, ind_2], g2.nodes)
+    np.testing.assert_allclose(nodes[:, : g1.num_nodes], g1.nodes)
+
+
 @pytest.mark.parametrize(
     "surface_coord, const",
     [
